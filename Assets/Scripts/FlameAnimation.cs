da221195faee10d4e3098c7f1130990c27a38b82
FruitRoute <51423653+FruitRoute@users.noreply.github.com>
2023-02-23 04:09:57 +0300

    Draft Created

---
+++ Assets/Scripts/FlameAnimation.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlameAnimation : MonoBehaviour
+{
+    public int 
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
